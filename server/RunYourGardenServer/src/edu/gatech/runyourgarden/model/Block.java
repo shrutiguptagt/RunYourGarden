ce77b8d4c664750717e21f89587915f43835d0a0
Shruti <g.shruti07@gmail.com>
2012-04-03 20:00:01 -0300

    Update server/RunYourGardenServer/src/edu/gatech/runyourgarden/model/Block.java

--- server/RunYourGardenServer/src/edu/gatech/runyourgarden/model/Block.java
+++ server/RunYourGardenServer/src/edu/gatech/runyourgarden/model/Block.java
@@ -1,6 +1,33 @@
 package edu.gatech.runyourgarden.model;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
+@PersistenceCapable
 public class Block {
-    int blockId;
+    @PrimaryKey
+    @Persistent
+	int blockId;
+	@Persistent
     int plantId;
+	
+	public void setPlantBlockId(int plantId,int blockId){
+		this.plantId = plantId;
+		this.blockId = blockId;
+	}
+
+	public int getPlantId() {
+        return plantId;
+    }
+    public void setPlantId(int plantId) {
+        this.plantId = plantId;
+    }
+    public int getBlockId() {
+        return blockId;
+    }
+    public void setBlockId(int blockId) {
+    	this.blockId = blockId;
+    }
+	
 }
